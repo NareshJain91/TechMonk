bfb863862c7e5c11550c1a65c4d36c10c87e7738
Vamsi Katragadda <vamsi.katragadda@publicissapient.com>
2019-10-09 11:37:20 +0530

    Change location of View Controller / View Model

---
+++ HRForecast/HRForecast/Dashboard/ViewModel/DashboardViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  DashboardViewModel.swift
+//  HRForecast
+//
+//  Created by Vamsi Katragadda on 09/10/19.
+//  Copyright © 2019 Sapient. All rights reserved.
+//
+
+import Foundation
+
+class DashboardViewModel: NSObject {
+    
+    override init() {
+        super.init()
+    }
+}
