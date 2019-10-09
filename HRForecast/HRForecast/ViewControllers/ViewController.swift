800023decd8a27e7f9c246c004123f986f3b44b1
Naresh Jain <narjain@publicisgroupe.net>
2019-10-09 08:39:20 +0530

    Initial Project Setup

---
+++ HRForecast/HRForecast/ViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HRForecast
+//
+//  Created by Naresh Jain on 09/10/19.
+//  Copyright © 2019 Sapient. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
