beaa3fe73b0cc5feeb5338cf865bd7c96d14a232
Naresh Jain <narjain@publicisgroupe.net>
2019-10-09 11:03:44 +0530

    Updated PodFile

--- HRForecast/HRForecast/ViewControllers/DashboardViewController/DashboardViewController.swift
+++ HRForecast/HRForecast/ViewControllers/DashboardViewController/DashboardViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  DashboardViewController.swift
 //  HRForecast
 //
 //  Created by Naresh Jain on 09/10/19.
@@ -8,13 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class DashboardViewController: UIViewController {
+    
+    var dashboardViewModel = DashboardViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    // MARK: - IBAction Methods
+    @IBAction func buttonAction(_ sender: UIButton) {
+        
+    }
 
 }
 
